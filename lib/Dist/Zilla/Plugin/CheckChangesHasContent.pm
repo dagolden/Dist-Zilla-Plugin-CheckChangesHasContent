fccc9b3cb063bb3318af2fb8875ecbbcb23316c8
David Golden <dagolden@cpan.org>
2010-05-12 09:17:19 -0400

    draft of CheckChangesHasContent

--- lib/Dist/Zilla/Plugin/CheckChangesHasContent.pm
+++ lib/Dist/Zilla/Plugin/CheckChangesHasContent.pm
@@ -1,16 +1,65 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::CheckChangesHasContent;
-# ABSTRACT: a module for CPAN
+# ABSTRACT: Ensure Changes has content before releasing
 
 # Dependencies
+use Dist::Zilla 2.100950 (); # XXX really the next release after this date
 use autodie 2.00;
+use File::pushd 0 ();
 use Moose 0.99;
 use namespace::autoclean 0.09;
 
 # extends, roles, attributes, etc.
 
+with 'Dist::Zilla::Role::BeforeRelease';
+
+has changelog => ( 
+  is => 'ro', 
+  isa => 'Str',
+  default => 'Changes' 
+);
+
 # methods
+
+sub before_release {
+  my $self = shift;
+  my $changes_file = $self->changelog;
+  my $newver = $self->zilla->version;
+
+  $self->zilla->ensure_built_in;
+  
+  # chdir in
+  my $wd = File::pushd::pushd($self->zilla->built_in);
+
+  # Must have Changes file
+  -e $changes_file or $self->log_fatal("No $changes_file found");
+
+  # Changes must have content
+  $self->_get_changes 
+    or $self->log_fatal("$changes_file has no content for $newver");
+
+  return;
+}
+
+# _get_changes copied and adapted from Dist::Zilla::Plugin::Git::Commit
+# by Jerome Quelin
+sub _get_changes {
+    my $self = shift;
+
+    # parse changelog to find commit message
+    my $changelog = Dist::Zilla::File::OnDisk->new( { name => $self->changelog } );
+    my $newver    = $self->zilla->version;
+    my @content   =
+        grep { /^$newver\s+/ ... /^\S/ } # from newver to un-indented
+        split /\n/, $changelog->content;
+    shift @content; # drop the version line
+    # drop unindented last line and trailing blank lines
+    pop @content while ( @content && $content[-1] =~ /^(?:\S|\s*$)/ );
+
+    # return number of non-blank lines
+    return scalar @content;
+} # end _get_changes
 
 __PACKAGE__->meta->make_immutable;
 
@@ -18,8 +67,7 @@ __PACKAGE__->meta->make_immutable;
 
 __END__
 
-=for Pod::Coverage::TrustPod
-  method_name_here
+=for Pod::Coverage before_release
 
 =begin wikidoc
 
