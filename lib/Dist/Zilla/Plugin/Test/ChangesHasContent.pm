79f8d39783badad8e9261fdd056c44a395710f8a
David Golden <xdg@xdg.me>
2016-05-25 15:10:53 -0400

    After release: bump $VERSION and timestamp Changes

--- lib/Dist/Zilla/Plugin/Test/ChangesHasContent.pm
+++ lib/Dist/Zilla/Plugin/Test/ChangesHasContent.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::Plugin::Test::ChangesHasContent;
 # ABSTRACT: Release test to ensure Changes has content
-our $VERSION = '0.010';
+our $VERSION = '0.011';
 
 # Dependencies
 use Dist::Zilla;
