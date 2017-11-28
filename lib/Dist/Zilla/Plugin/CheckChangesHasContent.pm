a1c1d90246b617c49e965fcce4e2f615bf0de233
David Golden <xdg@xdg.me>
2017-11-28 09:42:45 -0500

    After release: bump $VERSION and timestamp Changes

--- lib/Dist/Zilla/Plugin/CheckChangesHasContent.pm
+++ lib/Dist/Zilla/Plugin/CheckChangesHasContent.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::Plugin::CheckChangesHasContent;
 # ABSTRACT: Ensure Changes has content before releasing
-our $VERSION = '0.011';
+our $VERSION = '0.012';
 
 # Dependencies
 use Dist::Zilla 6 (); # XXX really the next release after this date
